4412681204f6d807a4cfc7cb16631702dafbab9c
eminbasol <eminbasol08@gmail.com>
2022-12-11 13:20:35 +0300

    Frontend UI & JavaScript

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/openai', require('./routes/openaiRoutes'));
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
